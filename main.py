8e5744798d6ad45e46e49e8e718f60f1ae04c46f
ellaklara <ellaklara.westerlund@gmail.com>
2020-11-28 19:50:47 +0100

    Add keystroke emulation and mappings

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from src.microphone import microphone
 from src.detect_intent import detect_intent_from_speech
 from src.intents import Intents
 
+from src.keystrokes import pressReleaseKey
+from src.nogba_mappings import NoGbaMappings
+
 def get_microphone_stream():
     microphone.ask_user_for_input_device()
     return microphone.get_microphone_stream()
@@ -17,6 +20,7 @@ def main():
 
         if intent == Intents.RUN:
             print("Run keyaction")
+            pressReleaseKey(NoGbaMappings.A)
         elif intent == Intents.BACK:
             print("Back keyaction")
         elif intent == Intents.FIGHT:
---
+++ src/nogba_mappings.py
@@ -0,0 +1,15 @@
+from src.keystrokes import HexKeyCodes
+
+class NoGbaMappings:
+    UP = HexKeyCodes.Q
+    DOWN = HexKeyCodes.A
+    LEFT = HexKeyCodes.O
+    RIGHT = HexKeyCodes.P
+    A = HexKeyCodes.SPACE
+    B = HexKeyCodes.TAB
+    SELECT = HexKeyCodes.CTRL
+    START = HexKeyCodes.ENTER
+    L = HexKeyCodes.ALT
+    R = HexKeyCodes.RIGHT_ALT
+    X = HexKeyCodes.X
+    Y = HexKeyCodes.Z
