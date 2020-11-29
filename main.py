9b736ed79b32c089ddfcf5e439e4ceeb04009f5a
Adam Jonsson <adamsmajl@gmail.com>
2020-11-29 16:44:58 +0100

    Change naming conventions for this project to camel case

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 
 from src.debug.text_input import debug_text_input
 from src.microphone import microphone
-from src.detect_intent import detect_intent_from_speech
+from src.detect_intent import detectIntentFromSpeech
 from src.intents import Intents
 
 from src.keystrokes import pressReleaseKey
 from src.nogba_mappings import NoGbaMappings, PokemonActions
 
 def get_microphone_stream():
-    microphone.ask_user_for_input_device()
-    return microphone.get_microphone_stream()
+    microphone.askUserForInputDevice()
+    return microphone.getMicrophoneStream()
 
 def main():
     microphone_stream = get_microphone_stream()
     while True:
         print(20 * "=")
         print("Listening for user action...")
-        intent, parameters = detect_intent_from_speech(microphone_stream)
+        intent, parameters = detectIntentFromSpeech(microphone_stream)
         pkmnActions = PokemonActions()
 
         if intent == Intents.GO:
--- src/detect_intent.py
+++ src/detect_intent.py
@@ -2,52 +2,52 @@ import dialogflow_v2 as dialogflow
 from .agent import agent;
 from .microphone import microphone
 
-def detect_intent_from_speech(microphone_stream):
-    audio_encoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
+def detectIntentFromSpeech(microphoneStream):
+    audioEncoding = dialogflow.enums.AudioEncoding.AUDIO_ENCODING_LINEAR_16
     sample_rate_hertz = microphone.RATE
 
-    def request_generator(audio_config):
-        query_input = dialogflow.types.QueryInput(audio_config=audio_config)
+    def requestGenerator(audioConfig):
+        queryInput = dialogflow.types.QueryInput(audio_config=audioConfig)
 
         # The first request contains the configuration.
         yield dialogflow.types.StreamingDetectIntentRequest(
-            session=agent.session, query_input=query_input)
+            session=agent.session, query_input=queryInput)
 
         while True:
-            chunk = microphone_stream.read(microphone.CHUNK)
+            chunk = microphoneStream.read(microphone.CHUNK)
             if not chunk:
                 break
             # The later requests contains audio data.
             yield dialogflow.types.StreamingDetectIntentRequest(
                 input_audio=chunk)
 
-    audio_config = dialogflow.types.InputAudioConfig(
-        audio_encoding=audio_encoding,
+    audioConfig = dialogflow.types.InputAudioConfig(
+        audio_encoding=audioEncoding,
         single_utterance=True,
         language_code=agent.language_code,
         sample_rate_hertz=sample_rate_hertz,
     )
 
-    requests = request_generator(audio_config)
+    requests = requestGenerator(audioConfig)
     responses = agent.session_client.streaming_detect_intent(requests)
 
     print('=' * 20)
-    last_response = None
+    lastResponse = None
     for response in responses:
-        last_response = response
+        lastResponse = response
         print('Intermediate transcript: "{}".'.format(
                 response.recognition_result.transcript))
 
     # Note: The result from the last response is the final transcript along
     # with the detected content.
-    query_result = last_response.query_result
+    queryResult = lastResponse.query_result
 
     print('=' * 20)
-    print('Query text: {}'.format(query_result.query_text))
+    print('Query text: {}'.format(queryResult.query_text))
     print('Detected intent: {} (confidence: {})\n'.format(
-        query_result.intent.display_name,
-        query_result.intent_detection_confidence))
+        queryResult.intent.display_name,
+        queryResult.intent_detection_confidence))
     print('Fulfillment text: {}\n'.format(
-        query_result.fulfillment_text))
+        queryResult.fulfillment_text))
 
-    return query_result.intent.display_name, query_result.parameters
+    return queryResult.intent.display_name, queryResult.parameters
