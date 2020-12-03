79e74ba04e08a75dd7d26dc5cba577afa2b619ae
ellaklara <ellaklara.westerlund@gmail.com>
2020-12-03 18:03:14 +0100

    Small changes, fix bug after editing Intents in console

--- src/helpers.py
+++ src/helpers.py
@@ -2,9 +2,8 @@ from src.nogba_mappings import NoGbaMappings
 from src.keystrokes import HexKeyCodes, pressReleaseKey
 
 class Helpers:
-    def goBackToMainMenuState(numOfBackActions=3, backActionDelay=0.2):
+    def goBackToMainMenuState(numOfBackActions=3, backActionDelay=0.1):
         for _ in range(numOfBackActions):
-            print("Menu back")
             pressReleaseKey(NoGbaMappings.B, sleep=backActionDelay)
         pressReleaseKey(NoGbaMappings.UP, sleep=0.2)
         pressReleaseKey(NoGbaMappings.LEFT, sleep=0.2)
